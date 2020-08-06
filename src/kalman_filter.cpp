f3d17eb4aa3bc325d3f04f149ff30ac9f89d08c6
ClarenceKuo <clarencekuo@gmail.com>
2020-08-06 17:36:30 +0800

    FEATURE: finish kalman filter

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,11 +3,6 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-/* 
- * Please note that the Eigen library does not initialize 
- *   VectorXd or MatrixXd objects with zeros upon creation.
- */
-
 KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
@@ -23,19 +18,51 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+  VectorXd y_ = z - H_ * x_;
+  MatrixXd S = H_ * P_ * H_.transpose() + R_;
+  MatrixXd K =  P_ * H_.transpose() * S.inverse();
+
+  // New state
+  x_ = x_ + (K * y_);
+  // New covariance
+  int x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+  VectorXd h = VectorXd(3);
+  double px = x_[0];
+  double py = x_[1];
+  double vx = x_[2];
+  double vy = x_[3];
+
+  double rho = sqrt(px * px + py * py);
+  double theta = atan2(py, px);
+  double rho_d = 0.000001;
+
+  if (rho != 0.0) {
+    rho_d = ( px * vx + py * vy ) / rho;
+  }
+  h << rho, theta, rho_d;
+
+  //EKF equaion
+  VectorXd y_ = z - h;
+  
+  y_[1] = atan2(sin(y_[1]), cos(y_[1]));
+  MatrixXd S = H_ * P_ * H_.transpose() + R_;
+  MatrixXd K = P_ * H_.transpose() * S.inverse();
+
+  // New state
+  x_ = x_ + (K * y_);
+  // New covariance
+  int x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
